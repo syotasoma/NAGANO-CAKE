31ed9e531e73ee49d2fe1c14082b5e0f2695d104
EC2 Default User <ec2-user@ip-172-31-35-22.ec2.internal>
2023-08-02 11:38:14 +0000

    11kaime

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -7,30 +7,39 @@ class Public::CartItemsController < ApplicationController
   def create
     @cart_item = CartItem.new(cart_item_params)
     @cart_item.customer_id = current_customer.id
-   if @cart_item.save
-    redirect_to cart_items_path
     # 1. 追加した商品がカート内に存在するかの判別
-    #存在した場合
-    #  2. カート内の個数をフォームから送られた個数分追加する
-    #存在しなかった場合
+     if cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]) 
+       #存在した場合
+       #  2. カート内の個数をフォームから送られた個数分追加する
+       cart_item.amount += params[:cart_item][:amount].to_i
+       cart_item.save
+       redirect_to cart_items_path
+      #存在しなかった場合
     #  カートモデルにレコードを新規作成する
-   end  
-  end
+     elsif @cart_item.save
+       redirect_to cart_items_path
+    
+     else
+       render 'customer/items/show'
+     end 
+  end 
+       
   
   def update
-    @cart_item = CartItem.find([:id])
-    @cart_item.update(curt_item_params)
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.update(cart_item_params)
     redirect_to cart_items_path
   end 
   
   def destroy
-   @items = item.find(params[:id])
-   item.destroy
+   cart_item = CartItem.find(params[:id])
+   cart_item.destroy
    redirect_to "/cart_items"
   end
   
   def all_destroy
-     @cart_items = CartItem.all
+     current_customer.cart_items.destroy_all
+     redirect_to "/cart_items"
   end
   
   private
